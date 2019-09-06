da979e576bb91a6d0fe32c867a22bc83b519b180
Felipe Machado <feelipe@email.com>
2019-09-05 21:22:52 -0300

    Titulo principal da area de cadastro (Dinâmico)

---
+++ src/objects/TitleCollab.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let title = 'title not defined'
+</script>
+
+<style>
+  .title-collab {
+    color: #f25a70;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+  }
+</style>
+
+<h1 class="title-collab -login">{title}</h1>

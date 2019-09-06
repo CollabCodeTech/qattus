a243ec75ed67351a8431334ca8e07c786e63a312
Felipe Machado <feelipe@email.com>
2019-09-05 21:25:49 -0300

    Incluido titulo principal e o label

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,32 @@
 <script>
   import FormCollab from './objects/FormCollab.svelte'
+  import TitleCollab from './objects/TitleCollab.svelte'
+  import LabelCollab from './objects/LabelCollab.svelte'
   import InputCollab from './objects/InputCollab.svelte'
   import ButtonCollab from './objects/ButtonCollab.svelte'
 </script>
 
 <FormCollab>
+  <TitleCollab title={'Faça seu cadastro abaixo'} />
+  <LabelCollab labelTitle={'Nome Completo'} />
   <InputCollab
     class="input-collab"
     type={'text'}
     name={'name'}
     placeholder={'Seu nome completo'} />
+  <LabelCollab labelTitle={'Email'} />
   <InputCollab
     class="input-collab"
     type={'email'}
     name={'email'}
     placeholder={'Seu email'} />
+  <LabelCollab labelTitle={'Senha'} />
   <InputCollab
     class="input-collab"
     type={'password'}
     name={'password'}
     placeholder={'Senha com no mínimo 8 digitos'} />
+  <LabelCollab labelTitle={'Confirmação de senha'} />
   <InputCollab
     class="input-collab"
     type={'password'}
